5f07fb178ca356a7b718baf8c4f3f4e7962a3bab
Krishna <krishna9842740994@gmail.com>
2026-08-09 21:55:17 +0530

    added project structure

---
+++ frontend/components/SignUp.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <div>
+    <button className="btn btn-primary">
+    Hello
+</button>
+    </div>
+  )
+}
+
+export default SignUp
---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    
+ <div className="App">
+     <button className="btn btn-primary">
+    Hello
+</button>
+    </div>
+  )
+}
+export default App;
