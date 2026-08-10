a79ac96f7c9c1ffce3108398d6ef90970292948a
Krishna <krishna9842740994@gmail.com>
2026-08-10 18:53:39 +0530

    user's input fetched

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,46 +1,70 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Login = () => {
+  const [user, setUser] = useState({
+    userName: "",
+    password: "",
+  });
+
+  const onSumbitHandler = (e) => {
+    e.preventDefault();
+    console.log(user);
+    setUser({
+      userName: "",
+      password: "",
+    });
+  };
   return (
-    <div className='min-w-96 mx-auto'>
-      <div className='h-full w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 border  border-gray-100'>
+    <div className="min-w-96 mx-auto">
+      <div className="h-full w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 border  border-gray-100">
+        <h1 className="text-3xl font-bold text-center text-gray-300">Login</h1>
 
-        <h1 className='text-3xl font-bold text-center text-gray-300'>Login</h1>
-
-        <form>
+        <form onSubmit={onSumbitHandler}>
           <div>
-            <label className='label p-2 '>
-              <span className='text-base label label-text'>Username</span>
+            <label className="label p-2 ">
+              <span className="text-base label label-text">Username</span>
             </label>
-            <input className='w-full input input-bordered h-10'
-            type='text' 
-            placeholder='Username'/>
+            <input
+              onChange={(e) => setUser({ ...user, userName: e.target.value })}
+              value={user.userName}
+              className="w-full input input-bordered h-10"
+              type="text"
+              placeholder="Username"
+            />
           </div>
           <div>
-            <label className='label p-2 '>
-              <span className='text-base label label-text'>Password</span>
+            <label className="label p-2 ">
+              <span className="text-base label label-text">Password</span>
             </label>
-            <input className='w-full input input-bordered h-10'
-            type='password' 
-            placeholder='Password  '/>
+            <input
+              onChange={(e) => setUser({ ...user, password: e.target.value })}
+              value={user.password}
+              className="w-full input input-bordered h-10"
+              type="password"
+              placeholder="Password"
+            />
           </div>
-    
-      <p className='text-center text-gray-300 my-2'>
 
+          <p className="text-center text-gray-300 my-2">
             Don't have an account?{" "}
-            <Link  className='text-cyan-400 hover:underline'to='/register'>  
-            SignUp
-          </Link>
-
+            <Link className="text-cyan-400 hover:underline" to="/register">
+              SignUp
+            </Link>
           </p>
           <div>
-            <button className='btn btn-block  mt-3 h-11 border border-slate-700'>Login</button>
+            <button
+              type="submit"
+              className="btn btn-block  mt-3 h-11 border border-slate-700"
+            >
+              Login
+            </button>
           </div>
-          </form>
+        </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -1,56 +1,93 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import { useState } from 'react'
 
 const SignUp = () => {
+
+  const [user,setUser]= useState({
+    fullName:'',
+    userName:'',
+    password:'',
+    confirmPassword:'',
+    gender:''
+  })
+
+  const handleCheckbox=(gender)=>{
+    setUser({...user,gender})
+  }
+
+  const onSumbitHandler=(e)=>{
+    e.preventDefault();
+    console.log(user);
+    setUser({
+      fullName:'',
+      userName:'',
+      password:'',
+      confirmPassword:'',
+      gender:''})
+  }
   return (
     <div className='min-w-96 mx-auto'>
       <div className='h-full w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 border  border-gray-100'>
 
         <h1 className='text-3xl font-bold text-center text-gray-300'>SignUp</h1>
 
-        <form>
+        <form onSubmit={onSumbitHandler}>
           <div>
             <label className='label p-2 '>
               <span className='text-base label label-text'>Full Name</span>
             </label>
-            <input className='w-full input input-bordered h-10'
+            <input onChange={(e)=>setUser({...user,fullName:e.target.value})}
+            value={user.fullName}
+             className='w-full input input-bordered h-10'
             type='text' 
-            placeholder='Full Name'/>
+            placeholder='Full Name' required/>
           </div>
           <div>
             <label className='label p-2 '>
               <span className='text-base label label-text'>Username</span>
             </label>
-            <input className='w-full input input-bordered h-10'
+            <input onChange={(e)=>setUser({...user,userName:e.target.value})}
+            value={user.userName}
+            className='w-full input input-bordered h-10'
             type='text' 
-            placeholder='Username'/>
+            placeholder='Username' required/>
           </div>
           <div>
             <label className='label p-2 '>
               <span className='text-base label label-text'>Password</span>
             </label>
-            <input className='w-full input input-bordered h-10'
+            <input onChange={(e)=>setUser({...user,password:e.target.value})}
+            value={user.password}
+            className='w-full input input-bordered h-10'
             type='password' 
-            placeholder='Password  '/>
+            placeholder='Password  ' required/>
           </div>
           <div>
             <label className='label p-2 '>
               <span className='text-base label label-text'>Confirm Password</span>
             </label>
-            <input className='w-full input input-bordered h-10'
+
+            <input onChange={(e)=>setUser({...user,confirmPassword:e.target.value})}
+            value={user.confirmPassword} 
+            className='w-full input input-bordered h-10'
             type='password' 
-            placeholder='Confirm Password'/>
+            placeholder='Confirm Password' required/>
           </div>
 
          <div className='flex items-center my-4 gap-4'>
           <div className='flex items-center'>
             <p>Male</p>
-            <input type="checkbox" defaultChecked className="checkbox mx-2" />
+            <input checked={user.gender==="male"}
+            onChange={()=>handleCheckbox("male")}
+            type="checkbox" defaultChecked className="checkbox mx-2"  />
           </div>
 
           <div className='flex items-center'>
             <p>Female</p>
-            <input type="checkbox" defaultChecked className="checkbox mx-2" />
+            <input checked={user.gender==="female"}
+            onChange={()=>handleCheckbox("female")}
+            type="checkbox" defaultChecked className="checkbox mx-2"  />
           </div>
          </div>
           <p className='text-center text-gray-300 my-2'>
@@ -63,7 +100,7 @@ const SignUp = () => {
 
           </p>
           <div>
-            <button className='btn btn-block  mt-3 h-11 border border-slate-700'>SignUp</button>
+            <button type='submit'className='btn btn-block  mt-3 h-11 border border-slate-700'>SignUp</button>
           </div>
           </form>
       </div>
